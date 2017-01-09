7ac4fb2c66aabb4dada38120ad8ec669aeaf3174
Bayan Bruss <cbbruss@gmail.com>
2017-01-09 18:40:08 +0000

    Fixed class bug

--- kfkd.py
+++ kfkd.py
@@ -12,43 +12,41 @@ import theano
 FTRAIN = "~/cnntest/data/training.csv"
 FTEST = "~/cnntest/data/test.csv"
 
-class FlipBatchIterator(BatchIterator):
-    flip_indices = [
-        (0, 2), (1, 3),
-        (4, 8), (5, 9), (6, 10), (7, 11),
-        (12, 16), (13, 17), (14, 18), (15, 19),
-        (22, 24), (23, 25),
-        ]
+# class FlipBatchIterator(BatchIterator):
+#     flip_indices = [
+#         (0, 2), (1, 3),
+#         (4, 8), (5, 9), (6, 10), (7, 11),
+#         (12, 16), (13, 17), (14, 18), (15, 19),
+#         (22, 24), (23, 25),
+#         ]
 
-    def transform(self, Xb, yb):
-        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+#     def transform(self, Xb, yb):
+#         Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
 
-        # Flip half of the images in this batch at random:
-        bs = Xb.shape[0]
-        indices = np.random.choice(bs, bs / 2, replace=False)
-        Xb[indices] = Xb[indices, :, :, ::-1]
+#         # Flip half of the images in this batch at random:
+#         bs = Xb.shape[0]
+#         indices = np.random.choice(bs, bs / 2, replace=False)
+#         Xb[indices] = Xb[indices, :, :, ::-1]
 
-        if yb is not None:
-            # Horizontal flip of all x coordinates:
-            yb[indices, ::2] = yb[indices, ::2] * -1
+#         if yb is not None:
+#             # Horizontal flip of all x coordinates:
+#             yb[indices, ::2] = yb[indices, ::2] * -1
 
-            # Swap places, e.g. left_eye_center_x -> right_eye_center_x
-            for a, b in self.flip_indices:
-                yb[indices, a], yb[indices, b] = (
-                    yb[indices, b], yb[indices, a])
+#             # Swap places, e.g. left_eye_center_x -> right_eye_center_x
+#             for a, b in self.flip_indices:
+#                 yb[indices, a], yb[indices, b] = (
+#                     yb[indices, b], yb[indices, a])
 
-        return Xb, yb
+#         return Xb, yb
 
 class AdjustVariable(object):
     def __init__(self, name, start=0.03, stop=0.001):
         self.name = name
         self.start, self.stop = start, stop
         self.ls = None
-
     def __call__(self, nn, train_history):
         if self.ls is None:
             self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
-
         epoch = train_history[-1]['epoch']
         new_value = float32(self.ls[epoch - 1])
         getattr(nn, self.name).set_value(new_value)
