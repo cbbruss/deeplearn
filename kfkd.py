6eb0965725641d8d3bd775d43c13fe4f0bc87a8a
Bayan Bruss <cbbruss@gmail.com>
2017-01-10 20:04:21 +0000

    created a 6th net with dropout

--- kfkd.py
+++ kfkd.py
@@ -12,32 +12,32 @@ import theano
 FTRAIN = "~/cnntest/data/training.csv"
 FTEST = "~/cnntest/data/test.csv"
 
-# class FlipBatchIterator(BatchIterator):
-#     flip_indices = [
-#         (0, 2), (1, 3),
-#         (4, 8), (5, 9), (6, 10), (7, 11),
-#         (12, 16), (13, 17), (14, 18), (15, 19),
-#         (22, 24), (23, 25),
-#         ]
+class FlipBatchIterator(BatchIterator):
+    flip_indices = [
+        (0, 2), (1, 3),
+        (4, 8), (5, 9), (6, 10), (7, 11),
+        (12, 16), (13, 17), (14, 18), (15, 19),
+        (22, 24), (23, 25),
+        ]
 
-#     def transform(self, Xb, yb):
-#         Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+    def transform(self, Xb, yb):
+        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
 
-#         # Flip half of the images in this batch at random:
-#         bs = Xb.shape[0]
-#         indices = np.random.choice(bs, bs / 2, replace=False)
-#         Xb[indices] = Xb[indices, :, :, ::-1]
+        # Flip half of the images in this batch at random:
+        bs = Xb.shape[0]
+        indices = np.random.choice(bs, bs / 2, replace=False)
+        Xb[indices] = Xb[indices, :, :, ::-1]
 
-#         if yb is not None:
-#             # Horizontal flip of all x coordinates:
-#             yb[indices, ::2] = yb[indices, ::2] * -1
+        if yb is not None:
+            # Horizontal flip of all x coordinates:
+            yb[indices, ::2] = yb[indices, ::2] * -1
 
-#             # Swap places, e.g. left_eye_center_x -> right_eye_center_x
-#             for a, b in self.flip_indices:
-#                 yb[indices, a], yb[indices, b] = (
-#                     yb[indices, b], yb[indices, a])
+            # Swap places, e.g. left_eye_center_x -> right_eye_center_x
+            for a, b in self.flip_indices:
+                yb[indices, a], yb[indices, b] = (
+                    yb[indices, b], yb[indices, a])
 
-#         return Xb, yb
+        return Xb, yb
 
 class AdjustVariable(object):
     def __init__(self, name, start=0.03, stop=0.001):
@@ -235,3 +235,56 @@ net4.fit(X, y)
 import cPickle as pickle
 with open('net4.pickle', 'wb') as f:
     pickle.dump(net4, f, -1)
+    
+
+net6 = NeuralNet(
+    layers=[
+        ('input', layers.InputLayer),
+        ('conv1', layers.Conv2DLayer),
+        ('pool1', layers.MaxPool2DLayer),
+        ('dropout1', layers.DropoutLayer),  # !
+        ('conv2', layers.Conv2DLayer),
+        ('pool2', layers.MaxPool2DLayer),
+        ('dropout2', layers.DropoutLayer),  # !
+        ('conv3', layers.Conv2DLayer),
+        ('pool3', layers.MaxPool2DLayer),
+        ('dropout3', layers.DropoutLayer),  # !
+        ('hidden4', layers.DenseLayer),
+        ('dropout4', layers.DropoutLayer),  # !
+        ('hidden5', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+    input_shape=(None, 1, 96, 96),
+    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+    dropout1_p=0.1,  # !
+    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+    dropout2_p=0.2,  # !
+    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
+    dropout3_p=0.3,  # !
+    hidden4_num_units=500,
+    dropout4_p=0.5,  # !
+    hidden5_num_units=500,
+    output_num_units=30, output_nonlinearity=None,
+
+    update_learning_rate=theano.shared(float32(0.03)),
+    update_momentum=theano.shared(float32(0.9)),
+
+    regression=True,
+    batch_iterator_train=FlipBatchIterator(batch_size=128),
+    on_epoch_finished=[
+        AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
+        AdjustVariable('update_momentum', start=0.9, stop=0.999),
+        ],
+    max_epochs=3000,
+    verbose=1,
+    )
+
+import sys
+sys.setrecursionlimit(10000)
+
+X, y = load2d()
+net6.fit(X, y)
+
+import cPickle as pickle
+with open('net6.pickle', 'wb') as f:
+    pickle.dump(net6, f, -1)
