8daa5072ba625614df5699848313f5afa6dc640a
Bayan Bruss <cbbruss@gmail.com>
2017-01-17 16:52:36 +0000

    added early stopping

--- kfkd.py
+++ kfkd.py
@@ -8,6 +8,8 @@ from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
 from nolearn.lasagne import BatchIterator
 import theano
+from collections import OrderedDict
+from sklearn.base import clone
 
 FTRAIN = "~/cnntest/data/training.csv"
 FTEST = "~/cnntest/data/test.csv"
@@ -51,6 +53,28 @@ class AdjustVariable(object):
         new_value = float32(self.ls[epoch - 1])
         getattr(nn, self.name).set_value(new_value)
 
+class EarlyStopping(object):
+    def __init__(self, patience=100):
+        self.patience = patience
+        self.best_valid = np.inf
+        self.best_valid_epoch = 0
+        self.best_weights = None
+
+    def __call__(self, nn, train_history):
+        current_valid = train_history[-1]['valid_loss']
+        current_epoch = train_history[-1]['epoch']
+        if current_valid < self.best_valid:
+            self.best_valid = current_valid
+            self.best_valid_epoch = current_epoch
+            self.best_weights = nn.get_all_params_values()
+        elif self.best_valid_epoch + self.patience < current_epoch:
+            print("Early stopping.")
+            print("Best valid loss was {:.6f} at epoch {}.".format(
+                self.best_valid, self.best_valid_epoch))
+            nn.load_params_from(self.best_weights)
+            raise StopIteration()
+
+
 def float32(k):
     return np.cast['float32'](k)
 
@@ -90,6 +114,86 @@ def load2d(test=False, cols=None):
     X = X.reshape(-1, 1, 96, 96)
     return X, y
 
+
+SPECIALIST_SETTINGS = [
+    dict(
+        columns=(
+            'left_eye_center_x', 'left_eye_center_y',
+            'right_eye_center_x', 'right_eye_center_y',
+            ),
+        flip_indices=((0, 2), (1, 3)),
+        ),
+
+    dict(
+        columns=(
+            'nose_tip_x', 'nose_tip_y',
+            ),
+        flip_indices=(),
+        ),
+
+    dict(
+        columns=(
+            'mouth_left_corner_x', 'mouth_left_corner_y',
+            'mouth_right_corner_x', 'mouth_right_corner_y',
+            'mouth_center_top_lip_x', 'mouth_center_top_lip_y',
+            ),
+        flip_indices=((0, 2), (1, 3)),
+        ),
+
+    dict(
+        columns=(
+            'mouth_center_bottom_lip_x',
+            'mouth_center_bottom_lip_y',
+            ),
+        flip_indices=(),
+        ),
+
+    dict(
+        columns=(
+            'left_eye_inner_corner_x', 'left_eye_inner_corner_y',
+            'right_eye_inner_corner_x', 'right_eye_inner_corner_y',
+            'left_eye_outer_corner_x', 'left_eye_outer_corner_y',
+            'right_eye_outer_corner_x', 'right_eye_outer_corner_y',
+            ),
+        flip_indices=((0, 2), (1, 3), (4, 6), (5, 7)),
+        ),
+
+    dict(
+        columns=(
+            'left_eyebrow_inner_end_x', 'left_eyebrow_inner_end_y',
+            'right_eyebrow_inner_end_x', 'right_eyebrow_inner_end_y',
+            'left_eyebrow_outer_end_x', 'left_eyebrow_outer_end_y',
+            'right_eyebrow_outer_end_x', 'right_eyebrow_outer_end_y',
+            ),
+        flip_indices=((0, 2), (1, 3), (4, 6), (5, 7)),
+        ),
+    ]
+
+def fit_specialists():
+    specialists = OrderedDict()
+
+    for setting in SPECIALIST_SETTINGS:
+        cols = setting['columns']
+        X, y = load2d(cols=cols)
+
+        model = clone(net)
+        model.output_num_units = y.shape[1]
+        model.batch_iterator_train.flip_indices = setting['flip_indices']
+        # set number of epochs relative to number of training examples:
+        model.max_epochs = int(1e7 / y.shape[0])
+        if 'kwargs' in setting:
+            # an option 'kwargs' in the settings list may be used to
+            # set any other parameter of the net:
+            vars(model).update(setting['kwargs'])
+
+        print("Training model for columns {} for {} epochs".format(
+            cols, model.max_epochs))
+        model.fit(X, y)
+        specialists[cols] = model
+
+    with open('net-specialists.pickle', 'wb') as f:
+        # we persist a dictionary with all models:
+        pickle.dump(specialists, f, -1)
 # X,y = load()
 # print("X.shape == {} X.min == {:.3f}; X.max == {:.3f}".format(X.shape,X.min(),X.max()))
 # print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(y.shape,y.min(),y.max()))
@@ -195,49 +299,101 @@ def load2d(test=False, cols=None):
 # with open('net3.pickle', 'wb') as f:
 #     pickle.dump(net3, f, -1)
 
-net4 = NeuralNet(
-    layers=[
-        ('input', layers.InputLayer),
-        ('conv1', layers.Conv2DLayer),
-        ('pool1', layers.MaxPool2DLayer),
-        ('conv2', layers.Conv2DLayer),
-        ('pool2', layers.MaxPool2DLayer),
-        ('conv3', layers.Conv2DLayer),
-        ('pool3', layers.MaxPool2DLayer),
-        ('hidden4', layers.DenseLayer),
-        ('hidden5', layers.DenseLayer),
-        ('output', layers.DenseLayer),
-        ],
-    input_shape=(None, 1, 96, 96),
-    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
-    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
-    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
-    hidden4_num_units=500, hidden5_num_units=500,
-    output_num_units=30, output_nonlinearity=None,
+# net4 = NeuralNet(
+#     layers=[
+#         ('input', layers.InputLayer),
+#         ('conv1', layers.Conv2DLayer),
+#         ('pool1', layers.MaxPool2DLayer),
+#         ('conv2', layers.Conv2DLayer),
+#         ('pool2', layers.MaxPool2DLayer),
+#         ('conv3', layers.Conv2DLayer),
+#         ('pool3', layers.MaxPool2DLayer),
+#         ('hidden4', layers.DenseLayer),
+#         ('hidden5', layers.DenseLayer),
+#         ('output', layers.DenseLayer),
+#         ],
+#     input_shape=(None, 1, 96, 96),
+#     conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+#     conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+#     conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
+#     hidden4_num_units=500, hidden5_num_units=500,
+#     output_num_units=30, output_nonlinearity=None,
 
-    update_learning_rate=theano.shared(float32(0.03)),
-    update_momentum=theano.shared(float32(0.9)),
+#     update_learning_rate=theano.shared(float32(0.03)),
+#     update_momentum=theano.shared(float32(0.9)),
 
-    regression=True,
-    # batch_iterator_train=FlipBatchIterator(batch_size=128),
-    on_epoch_finished=[
-        AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
-        AdjustVariable('update_momentum', start=0.9, stop=0.999),
-        ],
-    max_epochs=3000,
-    verbose=1,
-    )
+#     regression=True,
+#     # batch_iterator_train=FlipBatchIterator(batch_size=128),
+#     on_epoch_finished=[
+#         AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
+#         AdjustVariable('update_momentum', start=0.9, stop=0.999),
+#         ],
+#     max_epochs=3000,
+#     verbose=1,
+#     )
 
-X, y = load2d()
+# X, y = load2d()
 
-net4.fit(X, y)
+# net4.fit(X, y)
 
-import cPickle as pickle
-with open('net4.pickle', 'wb') as f:
-    pickle.dump(net4, f, -1)
+# import cPickle as pickle
+# with open('net4.pickle', 'wb') as f:
+#     pickle.dump(net4, f, -1)
     
 
-net6 = NeuralNet(
+# net6 = NeuralNet(
+#     layers=[
+#         ('input', layers.InputLayer),
+#         ('conv1', layers.Conv2DLayer),
+#         ('pool1', layers.MaxPool2DLayer),
+#         ('dropout1', layers.DropoutLayer),  # !
+#         ('conv2', layers.Conv2DLayer),
+#         ('pool2', layers.MaxPool2DLayer),
+#         ('dropout2', layers.DropoutLayer),  # !
+#         ('conv3', layers.Conv2DLayer),
+#         ('pool3', layers.MaxPool2DLayer),
+#         ('dropout3', layers.DropoutLayer),  # !
+#         ('hidden4', layers.DenseLayer),
+#         ('dropout4', layers.DropoutLayer),  # !
+#         ('hidden5', layers.DenseLayer),
+#         ('output', layers.DenseLayer),
+#         ],
+#     input_shape=(None, 1, 96, 96),
+#     conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+#     dropout1_p=0.1,  # !
+#     conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+#     dropout2_p=0.2,  # !
+#     conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
+#     dropout3_p=0.3,  # !
+#     hidden4_num_units=500,
+#     dropout4_p=0.5,  # !
+#     hidden5_num_units=500,
+#     output_num_units=30, output_nonlinearity=None,
+
+#     update_learning_rate=theano.shared(float32(0.03)),
+#     update_momentum=theano.shared(float32(0.9)),
+
+#     regression=True,
+#     batch_iterator_train=FlipBatchIterator(batch_size=128),
+#     on_epoch_finished=[
+#         AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
+#         AdjustVariable('update_momentum', start=0.9, stop=0.999),
+#         ],
+#     max_epochs=3000,
+#     verbose=1,
+#     )
+
+# import sys
+# sys.setrecursionlimit(10000)
+
+# X, y = load2d()
+# net6.fit(X, y)
+
+# import cPickle as pickle
+# with open('net6.pickle', 'wb') as f:
+#     pickle.dump(net6, f, -1)
+    
+net12 = NeuralNet(
     layers=[
         ('input', layers.InputLayer),
         ('conv1', layers.Conv2DLayer),
@@ -261,9 +417,9 @@ net6 = NeuralNet(
     dropout2_p=0.2,  # !
     conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
     dropout3_p=0.3,  # !
-    hidden4_num_units=500,
+    hidden4_num_units=1000,
     dropout4_p=0.5,  # !
-    hidden5_num_units=500,
+    hidden5_num_units=1000,
     output_num_units=30, output_nonlinearity=None,
 
     update_learning_rate=theano.shared(float32(0.03)),
@@ -274,8 +430,9 @@ net6 = NeuralNet(
     on_epoch_finished=[
         AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
         AdjustVariable('update_momentum', start=0.9, stop=0.999),
+        EarlyStopping(patience=200),
         ],
-    max_epochs=3000,
+    max_epochs=10000,
     verbose=1,
     )
 
@@ -283,8 +440,8 @@ import sys
 sys.setrecursionlimit(10000)
 
 X, y = load2d()
-net6.fit(X, y)
+net12.fit(X, y)
 
 import cPickle as pickle
-with open('net6.pickle', 'wb') as f:
-    pickle.dump(net6, f, -1)
+with open('net12.pickle', 'wb') as f:
+    pickle.dump(net12, f, -1)
